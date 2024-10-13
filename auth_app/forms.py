707f505d924ed4fa3e0c6e281dfab6a6271cce58
foy4748 <faisaljfcl@gmail.com>
2024-10-13 13:16:03 +0600

    Connected to POSTGRESQL. Added ENVs

--- auth_app/forms.py
+++ auth_app/forms.py
@@ -2,14 +2,27 @@ from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.models import User
 
+from auth_app.models import Profile
+
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={"id": "required"}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={"id": "required"}))
 
+    # Profile related fields
+    # Doesn't need it yet
+
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email"]
+
+    def save(self, commit=True):
+        current_user = super().save(commit=False)
+        if commit is True:
+            current_user.save()  # Saving User in DB
+            Profile.objects.create(user=current_user)
+
+        return current_user
 
 
 class ChangeProfileForm(UserChangeForm):
--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    balance = models.DecimalField(decimal_places=2, max_digits=12, default=0.00)
